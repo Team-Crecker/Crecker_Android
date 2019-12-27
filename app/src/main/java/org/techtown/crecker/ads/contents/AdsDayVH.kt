861c3fc64e4808a778e544b00ec679d00e1f7f18
박현준 <evoluti0n@hanyang.ac.kr>
2019-12-27 22:36:38 +0900

    AdsDetailActivity 뷰 구성 + 버그 픽스

--- app/src/main/java/org/techtown/crecker/ads/contents/AdsDayVH.kt
+++ app/src/main/java/org/techtown/crecker/ads/contents/AdsDayVH.kt
@@ -1,5 +1,6 @@
 package org.techtown.crecker.ads.contents
 
+import android.content.Intent
 import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
@@ -7,6 +8,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.techtown.crecker.R
+import org.techtown.crecker.ads.activity.AdsDetailActivity
 
 
 class AdsDayVH(view : View) : RecyclerView.ViewHolder(view){
@@ -26,5 +28,15 @@ class AdsDayVH(view : View) : RecyclerView.ViewHolder(view){
 
         if(data.dday!! > 7)
             tvDday.setBackgroundColor(Color.parseColor("#c9cdd2"))
+
+        itemView.setOnClickListener {
+            it.context.startActivity(
+                Intent(it.context, AdsDetailActivity::class.java)
+                    .apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        putExtra("title", data.title)
+                    }
+            )
+        }
     }
 }
