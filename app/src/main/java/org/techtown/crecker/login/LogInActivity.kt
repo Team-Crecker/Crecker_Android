86857d67723401b1bf75d027dee19025a2943cc1
jo0oy <rkfkrh07@naver.com>
2019-12-27 23:19:34 +0900

    로그인 화면 미완성

---
+++ app/src/main/java/org/techtown/crecker/login/LogInActivity.kt
@@ -0,0 +1,13 @@
+package org.techtown.crecker.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.techtown.crecker.R
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
