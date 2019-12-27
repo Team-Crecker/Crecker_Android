f8d37429dc1525fad138f2d0ed96b3502fff8ab7
박현준 <evoluti0n@hanyang.ac.kr>
2019-12-27 15:18:02 +0900

    캐시액티비티 - 카테고리 선택 바텀시트 구현

--- app/src/main/java/org/techtown/crecker/mypage/CashActivity.kt
+++ app/src/main/java/org/techtown/crecker/mypage/CashActivity.kt
@@ -1,10 +1,13 @@
 package org.techtown.crecker.mypage
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_cash.*
 import org.techtown.crecker.R
+import org.techtown.crecker.module.NavBarSetting
 import java.util.*
 
 class CashActivity : AppCompatActivity() {
@@ -46,5 +49,13 @@ class CashActivity : AppCompatActivity() {
         rv_usage.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         btn_goBack.setOnClickListener { finish() }
+
+        imageView4.setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+                .apply { setContentView(R.layout.filtering_dialog_layout) }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1)
+                NavBarSetting.setWhite(dialog)
+            dialog.show()
+        }
     }
 }
